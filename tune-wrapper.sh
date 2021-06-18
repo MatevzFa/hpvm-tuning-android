db207766315af0674f89b436fca60bf23c787e46
Matevž Fabjančič <matevz.fabjancic@gmail.com>
2021-06-18 17:08:29 +0200

    Use a on-node tmp dir for binaries

--- tune-wrapper.sh
+++ tune-wrapper.sh
@@ -12,4 +12,4 @@ script=$HOME/hpvm-tuning-android/tune_x.py
 cd $HOME/hpvm-tuning-android
 
 # singularity exec --nv --writable-tmpfs $hpvm_image python -u tune_x.py resnet18_cifar10 10000 3.0 5.0 -D tuning.resnet18_cifar10 -M tuner_results -B 500
-singularity exec --nv --writable-tmpfs $hpvm_image python -u tune_x.py "$@"
+singularity exec --nv --writable-tmpfs $hpvm_image python -u tune_x.py -D $(mktemp -ud) "$@"
