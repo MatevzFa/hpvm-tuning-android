dfa7e4ce56374998eb27e3cf6b22a24529b395d1
Matevž Fabjančič <matevz.fabjancic@gmail.com>
2021-06-22 15:26:40 +0200

    Implement model_inspection

--- model_inspection.py
+++ model_inspection.py
@@ -1,11 +1,11 @@
+import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
 import numpy as np
+from matplotlib.figure import Figure
+from torch import nn
+
 from tuning import *
-from torch import nn
 
-
-from torch import nn
 
 def plot_weights(model: nn.Module, conv_id: int) -> Figure:
     convs: "list[nn.Conv2d]" = []
@@ -14,18 +14,39 @@ def plot_weights(model: nn.Module, conv_id: int) -> Figure:
         if isinstance(m, nn.Conv2d):
             convs.append(m)
 
-    convs = [c for c in convs if c.kernel_size == (3,3)]
+    convs = [c for c in convs if c.kernel_size == (3, 3)]
 
     conv = convs[conv_id]
 
-    fig, _ = plt.subplots(4, 4)
+    fig, axes = plt.subplots(5, 5)
 
     filters = np.array(conv.weight.data)
     for ax, filter in zip(fig.axes, filters[:len(fig.axes)]):
-        ax.imshow(filter)
+        c, h, w = 0, 1, 2
+
+        f = np.transpose(filter, axes=[h, w, c])
+        # f = np.sum(f, axis=2)
+        f = (f - np.min(f))/np.ptp(f)
+
+        ax.imshow(f)
+        ax.set_frame_on(False)
+        ax.tick_params(
+            which='both',
+            bottom=False, top=False, left=False, right=False,
+            labelbottom=False, labeltop=False, labelleft=False, labelright=False,
+        )
+
+    for col, ax in enumerate(axes[0], ord('A')):
+        ax.set_title(chr(col))
+
+    for row, ax in enumerate(axes[:, 0], 1):
+        ax.set_ylabel(row, rotation=0, size='large', labelpad=10, y=.25)
+
+    print(filters.shape)
+    print(f"var(w) = {np.var(filters, axis=3).mean()}")
+    print(f"var(h) = {np.var(filters, axis=2).mean()}")
 
     return fig
-
 
 
 def main():
@@ -37,13 +58,10 @@ def main():
     model = info.model_factory()
     model.load_state_dict(torch.load(info.checkpoint))
 
-    print(model)
-
     fig = plot_weights(model, conv_id)
-    fig.set_title("2D Convolution no. {conv_id}")
+    # fig.suptitle(f"2D Convolution no. #{conv_id}")
     fig.set_size_inches(3, 3)
     fig.savefig(f'weights-{model_id}-conv{conv_id}.pdf', bbox_inches='tight')
-
 
 
 if __name__ == '__main__':
