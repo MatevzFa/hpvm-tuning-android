6c8833155481f9da1a55a74e8691b20c0f4a88b5
Matevž Fabjančič <matevz.fabjancic@gmail.com>
2021-07-30 16:16:18 +0200

    Document

--- analysis/pareto.py
+++ analysis/pareto.py
@@ -1,3 +1,4 @@
+from os import name
 import pathlib
 from analysis import setup_tex
 import argparse
@@ -13,6 +14,7 @@ from pathlib import Path
 class Args:
     conf_file: str
     parent: Optional[str]
+    profiled: Optional[str]
 
 
 def get_args():
@@ -20,6 +22,7 @@ def get_args():
 
     parser.add_argument("conf_file")
     parser.add_argument("--parent", type=str, default=None)
+    parser.add_argument("--profiled", type=str, default=None)
 
     return Args(**vars(parser.parse_args()))
 
@@ -39,13 +42,16 @@ def main():
     _, configs = read_hpvm_configs(conf_file)
     orig_len = len(configs)
 
+    # Sort configurations according to QoS Loss
     configs = list(sorted(configs, key=lambda c: c.qos_loss))
+    # and remove those that have negative QoS loss (insensible result)
     configs = list(filter(lambda c: c.qos_loss >= 0, configs))
 
     print(f"removed {orig_len - len(configs)} outright")
 
+    # Construct a sensible pareto frontier of approximations.
+    # Only accept a new configuration if it yields a speedup over the previous one
     pareto_front = [configs[0]]
-
     for c in configs[1:]:
         if c.speedup - pareto_front[-1].speedup > 1e-2:
             print(f"diff = {c.speedup - pareto_front[-1].speedup}")
@@ -55,12 +61,14 @@ def main():
 
     pareto_file_base = Path(args.parent or conf_file.parent) / (conf_file.stem + '.pareto')
 
-    plt.figure(figsize=(5, 2.5))
-    plot_configs(pareto_front, 'b:')
-    plot_configs(configs, 'r-')
-    plot_configs(pareto_front, 'b^')
+    plt.figure(figsize=(5.5, 3))
+    plot_configs(pareto_front, 'b.:', label="Pareto frontier")
+    plot_configs(configs, 'r-', label="All configurations (ApproxTuner)")
+    plot_configs(pareto_front, 'b.')
+
     plt.xlabel("QoS Loss [\% points]")
     plt.ylabel("Speedup")
+    plt.legend(loc='best', fontsize='small')
     plt.savefig(str(pareto_file_base)+".pdf", bbox_inches='tight')
 
     for i, p in enumerate(pareto_front):
